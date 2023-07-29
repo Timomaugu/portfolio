51d82c07f9c90d82fc544edd4c78aa711c72d684
Timomaugu <timomaugu@gmail.com>
2023-07-30 02:40:57 +0300

    static version

--- includes/navbar.php
+++
@@ -1,6 +0,0 @@
-<nav class="navbar">
-    <a href="../page/home.php"> <i class="fas fa-home"></i> <span>HOME</span> </a>
-    <a href="../page/about.php"> <i class="fas fa-user"></i> <span>ABOUT</span> </a>
-    <a href="../page/portfolio.php"> <i class="fas fa-briefcase"></i> <span>PORTFOLIO</span> </a>
-    <a href="../page/contact.php"> <i class="fas fa-address-book"></i> <span>CONTACT</span> </a>
-</nav>
